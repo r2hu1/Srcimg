c0ec0c26f6624e2cff27f6bd702ce0cb88d56db1
r2hu1 <newasram86@gmail.com>
2023-12-09 16:37:06 +0530

    #i

--- app/layout.js
+++ app/layout.js
@@ -7,10 +7,10 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Srcimg',
   description: 'Get direct links of your images!',
-  // icons: {
-  //   icon: '/favicon.ico',
-  // },
-}
+  icons: {
+    icon: 'https://i.postimg.cc/HsmwGfkH/Screenshot-2023-12-08-235237.png',
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
