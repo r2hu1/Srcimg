70f60b5db1d1823d2bda613349535795d6f35586
r2hu1 <newasram86@gmail.com>
2023-12-10 19:54:21 +0530

    #df

--- app/manage/page.jsx
+++ app/manage/page.jsx
@@ -6,26 +6,44 @@ import { useEffect, useState } from "react";
 import { FaRegCopy } from "react-icons/fa6";
 import { useSession } from "next-auth/react";
 import { BiLoaderAlt } from "react-icons/bi";
+import { useToast } from "@/components/ui/use-toast";
+
+
 
 const Manage = () => {
-    const [files, setFiles] = useState([]);
+
     // https://res.cloudinary.com/ddv7gse97/image/upload/
 
-    const { data: userData } = useSession();
-    const getFile = () => {
-        fetch("/api/file/get", {
+    const [files, setFiles] = useState([]);
+    const [clicked, setClicked] = useState(false);
+    const { toast } = useToast();
+    const { data } = useSession();
+
+    const getFile = async () => {
+        toast({
+            title: "Loading...",
+            description: "Please wait while we load your files.",
+            variant: "default",
+            isLoading: true
+        })
+        setClicked(true);
+        const res = await fetch("/api/file/get", {
             method: "POST",
-            body: JSON.stringify({ email: userData.user.email }),
+            body: JSON.stringify({ email: data?.user?.email }),
             headers: {
                 "Content-Type": "application/json"
             }
-        })
-            .then(res => res.json())
-            .then(data => {
-                setFiles([...data]);
-                console.log(files);
-            });
+        });
+
+        const respData = await res.json();
+        let gotFiles = [];
+        for(let i = 0; i < respData.length; i++) {
+            gotFiles.push(respData[i].files);
+            files.push(`https://res.cloudinary.com/ddv7gse97/image/upload${respData[i].files}`);
+        }
+        console.log(files);
     };
+
 
     return (
         <section className="mt-20 mb-20">
@@ -34,18 +52,18 @@ const Manage = () => {
                     <Link href="/" className="hover:underline hover:text-primary">Srcimg</Link> <span className="text-lg font-thin">/</span> Manage
                 </div>
                 <div className="mt-7">
-                    {files.length < 1 ? (
+                    {!clicked ? (
                         <div>
-                            <div className="mt-4 h-[120px] w-full border rounded flex justify-center items-center text-sm"><Button variant="link" className="no-underline" onClick={getFile}>Click To Refresh <BiLoaderAlt className="h-5 w-5 animate-spin ml-2"/></Button></div>
+                            <div className="mt-4 h-[120px] w-full border rounded flex justify-center items-center text-sm"><Button variant="link" className="no-underline" onClick={getFile}>Click To Refresh <BiLoaderAlt className="h-5 w-5 animate-spin ml-2" /></Button></div>
                         </div>
                     ) : (
                         <div className="flex flex-wrap gap-2">
                             {
                                 files.map((file, index) => (
                                     <div key={index} className="rounded-lg w-full p-3">
-                                        <img className="p-1 rounded-lg h-40 w-full border mb-3" src={`https://res.cloudinary.com/ddv7gse97/image/upload${file.url}`} alt={file.name} />
+                                        <img className="p-1 rounded-lg h-40 w-full border mb-3" src={file} alt={"Image"} />
                                         <div className="flex gap-1 w-full">
-                                            <Button className="w-full flex gap-1 items-center justify-center outline-none">Copy Link<FaRegCopy className="h-[15px] w-[15px]" /></Button>
+                                            <Button className="w-full flex gap-1 items-center justify-center outline-none" onClick={() => {navigator.clipboard.writeText(file); toast({title: "Link Copied"});}}>Copy Link<FaRegCopy className="h-[15px] w-[15px]" /></Button>
                                         </div>
                                     </div>
                                 ))
